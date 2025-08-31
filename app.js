5e6d40aa1cccd8a25d951a21e078538c8b8369ec
Denis Carvalho <deniscdlima@gmail.com>
2025-08-31 01:23:15 -0300

    finalizando logica

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+// 01. Função para adicionar amigo
+function adicionarAmigo() {
+    console.log(amigos);
+    let input = document.getElementById("amigo");
+    let nome = input.value.trim(); // pega o valor do input e remove espaços
+
+    // 02. Validar entrada
+    if (nome === "") {
+        alert("Por favor, insira um nome válido.");
+        return;
+    }
+
+    // 03. Atualizar o array
+    amigos.push(nome);
+
+    // 04. Limpar o campo de entrada
+    input.value = "";
+
+    // 05. Atualizar a lista na tela
+    atualizarLista();
+}
+
+// 05. Função para atualizar lista de amigos
+function atualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; // limpa a lista atual
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
+
+// 06. Função para sortear amigo
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Adicione pelo menos um amigo antes de sortear");
+        return;
+    }
+
+    let indiceSorteado = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceSorteado];
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>O amigo secreto sorteado foi: <strong>${amigoSorteado}</strong></li>`;
+}
+
+
+
+
